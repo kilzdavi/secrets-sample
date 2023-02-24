58268e778405c7171b71ac8958881e7e02f32a0f
kilzdavi <kilzdavi@amazon.com>
2023-02-24 21:23:46 +0000

    Added Pagination to Book index list.

--- BookStoreCore/Pages/Books/Index.cshtml.cs
+++ BookStoreCore/Pages/Books/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,24 +13,39 @@ namespace BookStoreCore.Pages.Books
 {
     public class IndexModel : PageModel
     {
-        private readonly BookStoreCore.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(BookStoreCore.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         public string NameSort { get; set; }
-        public string DateSort { get; set; }
+        //public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Book> Books { get;set; } = default!;
+        public PaginatedList<Book> Books { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+
+        //public IList<Book> Books { get;set; } = default!;
+
+        public async Task OnGetAsync(string sortOrder, string searchString, string currentFilter, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             //DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if(searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
             IQueryable<Book> booksIQ = from b in _context.Books
                                       select b;
@@ -50,9 +66,10 @@ namespace BookStoreCore.Pages.Books
 
             if (_context.Books != null)
             {
-                Books = await booksIQ.AsNoTracking()
+                var pageSize = Configuration.GetValue("PageSize", 4);
+                Books = await PaginatedList<Book>.CreateAsync(booksIQ.AsNoTracking()
                 .Include(b => b.Category)
-                .Include(b => b.Publisher).ToListAsync();
+                .Include(b => b.Publisher), pageIndex ?? 1, pageSize);
             }
         }
     }
