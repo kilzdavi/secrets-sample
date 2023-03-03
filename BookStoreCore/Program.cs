af583f608f00a3e236084975543e0904514850b4
kilzdavi <kilzdavi@amazon.com>
2023-03-02 21:13:23 -0600

    Updated adot-otel collector configuration. Cleaned up Program.cs

--- BookStoreCore/Program.cs
+++ BookStoreCore/Program.cs
@@ -15,55 +15,57 @@ using OpenTelemetry.Contrib.Extensions.AWSXRay.Trace;
 using System.Net.Http;
 
 // Define some important constants to initialize tracing with
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+#region OpenTelemetry
 var serviceName = "AWS.SampleApp.BookStoreCore";
 var serviceVersion = "1.0.0";
 var MyActivitySource = new ActivitySource(serviceName);
 
-
-var builder = WebApplication.CreateBuilder(args);
-
 var appResourceBuilder = ResourceBuilder.CreateDefault()
-    .AddService(serviceName: serviceName, serviceVersion: serviceVersion);
+        .AddService(serviceName: serviceName, serviceVersion: serviceVersion);
 
-// Configure important OpenTelemetry settings, the console exporter, and instrumentation library
-builder.Services.AddOpenTelemetry().WithTracing(tracerProviderBuilder =>
-{
-    tracerProviderBuilder
-        .AddXRayTraceId()
-        .AddAWSInstrumentation()
-        //.AddConsoleExporter()
-        .AddOtlpExporter(opt =>
-        {
-            opt.Protocol = OtlpExportProtocol.HttpProtobuf;
-        })
-        .AddSource(serviceName)
-        .SetResourceBuilder(
-            ResourceBuilder.CreateDefault()
-                .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddSqlClientInstrumentation();
+    // Configure important OpenTelemetry settings, the console exporter, and instrumentation library
+    //builder.Services.AddOpenTelemetry().WithTracing(tracerProviderBuilder =>
+    //{
+    //    tracerProviderBuilder
+    //        //.AddXRayTraceId()
+    //        //.AddAWSInstrumentation()
+    //        //.AddConsoleExporter()
+    //        .AddOtlpExporter(opt =>
+    //        {
+    //            opt.Protocol = OtlpExportProtocol.HttpProtobuf;
+    //        })
+    //        .AddSource(serviceName)
+    //        .SetResourceBuilder(
+    //            ResourceBuilder.CreateDefault()
+    //                .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+    //        .AddHttpClientInstrumentation()
+    //        .AddAspNetCoreInstrumentation()
+    //        .AddSqlClientInstrumentation();
+
+    //});
 
 
-});
+    var meter = new Meter(serviceName);
+    var counter = meter.CreateCounter<long>("app.request-counter");
 
+    builder.Services.AddOpenTelemetry().WithMetrics(metricProviderBuilder =>
+    {
+        metricProviderBuilder
+            .AddPrometheusExporter(options =>
+            {
+                options.ScrapeResponseCacheDurationMilliseconds = 0;
+            })
+            .AddMeter(meter.Name)
+            .SetResourceBuilder(appResourceBuilder)
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+    });
 
-var meter = new Meter(serviceName);
-var counter = meter.CreateCounter<long>("app.request-counter");
-
-builder.Services.AddOpenTelemetry().WithMetrics(metricProviderBuilder =>
-{
-    metricProviderBuilder
-        .AddPrometheusExporter(options =>
-        {
-            options.ScrapeResponseCacheDurationMilliseconds = 0;
-        })
-        .AddMeter(meter.Name)
-        .SetResourceBuilder(appResourceBuilder)
-        .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation();
-});
-
+#endregion
 
 // Add Additional services to the container.
 builder.Services.AddRazorPages();
@@ -72,11 +74,31 @@ builder.Services.AddDbContext<ApplicationDbContext>(
         builder.Configuration.GetConnectionString("DefaultConnection")
         )); //Dependency Injection for DB Context
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
-
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.Configure<IdentityOptions>(options =>
+#region Identity
+    builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
+    builder.Services.Configure<IdentityOptions>(options =>
+    {
+        // Password settings.
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+
+        // Lockout settings.
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+
+        // User settings.
+        options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        options.User.RequireUniqueEmail = false;
+    });
+    builder.Services.Configure<IdentityOptions>(options =>
 {
     // Password settings.
     options.Password.RequireDigit = true;
@@ -96,28 +118,7 @@ builder.Services.Configure<IdentityOptions>(options =>
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
     options.User.RequireUniqueEmail = false;
 });
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
-
-    // User settings.
-    options.User.AllowedUserNameCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
-});
-
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -153,6 +154,9 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+//Config Otel Endpoint for Prometheus
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
+
 app.MapGet("/test", async () =>
 {
     var httpClient = new HttpClient();
@@ -160,23 +164,6 @@ app.MapGet("/test", async () =>
     if (string.IsNullOrWhiteSpace(html))
     {
         return "Hello, World!";
-    }
-    else
-    {
-        return "Hello, World!";
-    }
-});
-
-app.UseOpenTelemetryPrometheusScrapingEndpoint();
-
-
-app.MapGet("/hello", async () =>
-{
-    var httpClient = new HttpClient();
-    var html = await httpClient.GetStringAsync("https://example.com/");
-    if (string.IsNullOrWhiteSpace(html))
-    {
-        return $"Hello, World!";
     }
     else
     {
