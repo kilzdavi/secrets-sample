67b359a59f60c695087137f5ca07bea518024e4d
kilzdavi <kilzdavi@amazon.com>
2023-03-10 10:52:42 -0600

    Added customTrace tag to custom trace.

--- BookStoreCore/Pages/Index.cshtml.cs
+++ BookStoreCore/Pages/Index.cshtml.cs
@@ -14,15 +14,27 @@ namespace BookStoreCore.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _config = configuration;
+            MyActivitySource = new ActivitySource(_config.GetSection("Observability")["ServiceName"], _config.GetSection("Observability")["Version"]);
         }
+
+        public static ActivitySource MyActivitySource { get; set; }
 
         public void OnGet()
         {
 
+            using var activity = MyActivitySource.StartActivity("VisitHome", ActivityKind.Server); // this will be translated to a X-Ray Segment
+            activity?.SetTag("http.method", "GET");
+            activity?.SetTag("http.url", "http://www.bookstorecore.com");            
+            activity?.SetTag("http.page", "Home");
+            activity?.SetTag("CustomTrace", "true");
+            activity?.AddEvent(new("A user visited the site!")); //_Logger is recommened for high volumes of events. 
+            
         }
 
         public void StartTrace()
@@ -30,6 +42,16 @@ namespace BookStoreCore.Pages
 
         }
 
-       
+
     }
+
+    public class Telmetry
+    {
+
+        public Telmetry()
+        {
+
+        }
+    }
+
 }
