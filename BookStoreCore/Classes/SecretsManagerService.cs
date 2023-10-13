bfb3b871bf1fdb35882ef7ea6fde8cbd1d674705
David Kilzer <kilzdavi@amazon.com>
2023-10-13 09:55:59 -0500

    Added Caching to the SecretsManagerService.cs

--- BookStoreCore/Classes/SecretsManagerService.cs
+++ BookStoreCore/Classes/SecretsManagerService.cs
@@ -1,18 +1,34 @@
 using Amazon.SecretsManager;
+using Amazon.SecretsManager.Extensions.Caching;
 using Amazon.SecretsManager.Model;
 
 namespace BookStoreCore.Classes;
 
 public class  SecretsManagerService
 {
-    private readonly AmazonSecretsManagerClient _client = new AmazonSecretsManagerClient();
-    
-    public async Task<string> GetSecretAsync(string secretId)
+    private AmazonSecretsManagerClient _client { get; set; }
+    private static SecretCacheConfiguration cacheConfiguration = new SecretCacheConfiguration
     {
-        var request = new GetSecretValueRequest { SecretId = secretId };
-        var response = await _client.GetSecretValueAsync(request);
+        CacheItemTTL = 30000 
+        //3600000 1 hour
+    };
+    public SecretsManagerCache _cache { get; set; }
+
+    public SecretsManagerService()
+    {
+        _client = new AmazonSecretsManagerClient();
+        _cache = new SecretsManagerCache(_client, cacheConfiguration);
+    }
+
+    public async Task<string> GetSecretAsync(string secretName)
+    {
         
-        return response.SecretString;
+        // var request = new GetSecretValueRequest { SecretId = secretName };
+        // var response = await _client.GetSecretValueAsync(request);
+
+        var mySecret = await _cache.GetSecretString(secretName);
+        Console.WriteLine(_cache);
+        return mySecret;
     }
     
 }
